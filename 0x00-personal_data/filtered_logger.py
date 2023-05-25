8c2ae373239321311bfda8a6622b67f7ef9b9ef1
Popsicool <akinolasamson1234@gmail.com>
2023-05-25 22:06:35 +0100

    edit file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,6 +33,9 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        '''
+        initialize
+        '''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
