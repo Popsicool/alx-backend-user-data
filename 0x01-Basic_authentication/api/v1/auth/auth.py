542c141332b819977f345b0f9d038be7be8fb1d8
Popsicool <akinolasamson1234@gmail.com>
2023-05-30 10:13:59 +0100

    initial commit

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+class to manage the API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''
+    class Auth
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        require auth
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) < 1:
+            return True
+        if (path + "/") in excluded_paths:
+            return False
+        if path not in excluded_paths:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''
+        authorization header
+        '''
+        if request is None or request.headers.get("Authorization") is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+        current user
+        '''
+        return None
