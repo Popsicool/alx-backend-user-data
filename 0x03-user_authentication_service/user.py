6e40886b7d67c78afb517ce3124bc3b530e5641c
Popsicool <akinolasamson1234@gmail.com>
2023-06-06 10:27:42 +0100

    modified user file

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,6 +15,7 @@ class User(Base):
     user model
     '''
     __tablename__ = "user"
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
